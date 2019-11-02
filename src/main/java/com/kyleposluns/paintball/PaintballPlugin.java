4abc93e15ac0a1601d432ec55008fd7f282c80d7
kyleposluns <kyleposluns@gmail.com>
2019-11-02 01:48:04 -0400

    shell plugin.

--- src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
+++ src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
@@ -4,14 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaintballPlugin extends JavaPlugin {
 
+  public PaintballPlugin() {
+    // empty constructor. This class' constructor is called by the server.
+  }
+
   @Override
   public void onEnable() {
-
+    this.getLogger().info("The plugin has successfully loaded.");
   }
 
   @Override
   public void onDisable() {
-
+    this.getLogger().info("The plugin has successfully unloaded.");
   }
 
 }

5c8181db4c3e1880261cb8624545e7ffb82b19e2
kyleposluns <kyleposluns@gmail.com>
2019-11-02 01:42:39 -0400

    Added info in README.md.

---
+++ src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
@@ -0,0 +1,17 @@
+package com.kyleposluns.paintball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaintballPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+
+}

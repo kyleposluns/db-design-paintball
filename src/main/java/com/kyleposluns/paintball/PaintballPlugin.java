754d3a5d3f075001799f79fe49571e50cb05fd6c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-19 15:58:12 -0500

    Added test features

--- src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
+++ src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
@@ -1,8 +1,14 @@
 package com.kyleposluns.paintball;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PaintballPlugin extends JavaPlugin {
+public class PaintballPlugin extends JavaPlugin implements Listener{
 
   public PaintballPlugin() {
     // empty constructor. This class' constructor is called by the server.
@@ -12,11 +18,22 @@ public class PaintballPlugin extends JavaPlugin {
   public void onEnable() {
     System.out.println("This is a test.");
     this.getLogger().info("The plugin has successfully loaded.");
+    this.getServer().getPluginManager().registerEvents(this, this);
   }
 
   @Override
   public void onDisable() {
     this.getLogger().info("The plugin has successfully unloaded.");
   }
-
+  
+  @EventHandler
+  public void onJoin(PlayerJoinEvent event) {
+    Bukkit.broadcastMessage("Welcome");
+  }
+  
+  @EventHandler
+  public void onMove(PlayerMoveEvent event) {
+    Bukkit.broadcastMessage("you moved");
+  }
+  
 }

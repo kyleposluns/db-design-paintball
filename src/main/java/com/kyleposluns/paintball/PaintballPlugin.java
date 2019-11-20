d13ff24ab2e789e1c93e7c409f0371033141797a
Sean <seank1@rochester.rr.com>
2019-11-20 15:57:26 -0500

    connection stuff

--- src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
+++ src/main/java/com/kyleposluns/paintball/PaintballPlugin.java
@@ -1,21 +1,103 @@
 package com.kyleposluns.paintball;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class PaintballPlugin extends JavaPlugin {
 
+  private String userName;
+
+  private String password;
+
+  private String serverName;
+
+  private int portNumber;
+
+  private Scanner s;
+
+  /** The name of the database we are testing with (this default is installed with MySQL) */
+  private final String dbName = "lotrfinalKolczynskiS";
+
+  private File customConfigFile;
+  private FileConfiguration customConfig;
+
   public PaintballPlugin() {
     // empty constructor. This class' constructor is called by the server.
+    this.createCustomConfig();
   }
 
   @Override
   public void onEnable() {
     this.getLogger().info("The plugin has successfully loaded.");
+    Connection conn = this.getConnection();
+    try {
+      this.initTables(conn);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public void onDisable() {
     this.getLogger().info("The plugin has successfully unloaded.");
+  }
+
+
+  private Connection getConnection() {
+    Connection conn = null;
+    try {
+      Scanner s = new Scanner(this.customConfigFile);
+      Properties connectionProps = new Properties();
+      connectionProps.load(new FileReader(customConfigFile));
+      connectionProps.put("user", connectionProps.getProperty("username"));
+      connectionProps.put("password", connectionProps.getProperty("password"));
+      conn = DriverManager.getConnection("jdbc:mysql://"
+                      + connectionProps.getProperty("serverName")  + ":"
+                      + connectionProps.getProperty("port") + "/"
+                      + connectionProps.getProperty("dbName")
+                      + "?characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true",
+              connectionProps);
+    } catch (SQLException | FileNotFoundException sql) {
+      System.out.println("Incorrect username and/or password. Try again.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return conn;
+  }
+
+  private void createCustomConfig() {
+    customConfigFile = new File(getDataFolder(), "custom.yml");
+    if (!customConfigFile.exists()) {
+      customConfigFile.getParentFile().mkdirs();
+      saveResource("custom.yml", false);
+    }
+
+    customConfig= new YamlConfiguration();
+    try {
+      customConfig.load(customConfigFile);
+    } catch (IOException | InvalidConfigurationException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void initTables(Connection conn) throws SQLException {
+    Statement beingTable;
+    Statement playerTable;
+
+    beingTable = conn.createStatement();
   }
 
 }
---
+++ src/main/java/com/kyleposluns/paintball/arena/BasicArena.java
@@ -0,0 +1,28 @@
+package com.kyleposluns.paintball.arena;
+
+import org.bukkit.Location;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BasicArena implements Arena {
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public UUID getUniqueId() {
+    return null;
+  }
+
+  @Override
+  public Region bounds() {
+    return null;
+  }
+
+  @Override
+  public List<Location> getSpawns() {
+    return null;
+  }
+}

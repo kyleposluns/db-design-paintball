ae22e869f0ef45aa192dfb96d0a12032dbf36b9e
Sean <seank1@rochester.rr.com>
2019-12-03 21:30:58 -0500

    fnuhuihu

--- src/main/java/com/kyleposluns/paintball/sql/AddToKillTable.java
+++ src/main/java/com/kyleposluns/paintball/sql/AddToKillTable.java
@@ -8,7 +8,8 @@ public class AddToKillTable extends AbstractSQLCommand {
   public AddToKillTable(Connection conn, int wave, UUID playID, String mon) {
     super(conn);
     this.command =
-            "CALL AddKillTable( " + "'" + playID.toString() + "' " + ", " + mon + ", " + wave +
+            "CALL AddKillTable( " + "'" + playID.toString() + "' " + ", " + "'" + mon + "' " + "," +
+                    " " + wave +
             ");";
   }
 }
